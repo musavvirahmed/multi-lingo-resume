1e5eb19ba91d6a5777a8d2bc46a94760019dbc3f
musavvirahmed <musavvir@gmail.com>
2018-07-28 17:09:51 +0800

    First commit
    Possible next steps-
    • clean up unnecessary files
    • delete unnecessary styling
    • minify everything

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# multi-lingo-resume
-Minimal resume template with multiple language support
+## Multiple Lingo Resume
+
+• I'm just practicing front-end trickery plus some Chinese language vocab.  
+• Feel free to fork it, hack and do whatever you want.  
+• Much gratitude to [Chloe](https://www.linkedin.com/in/chloe-hung-a516b8b9/ "Chloe Hung, UX Designer") for proof reading the Mandarin.  
---
+++ js/custom.js
@@ -0,0 +1,55 @@
+$(function(){
+  $(".typed").typed({
+    strings: ["digital product designer", "UX designer", "student of economics", "student of Mandarin", "Bangladeshi", "horrible swimmer"],
+    typeSpeed: 50,
+    backDelay: 2500,
+    loop: true,
+    shuffle: false
+  });
+  $(".typedbn").typed({
+    strings: ["ডিজিটাল প্রোডাক্ট ডিজাইনার", "ইউ-এক্স ডিজাইনার", "অর্থনীতির ছাত্র", "ম্যান্ডারিন ভাষার ছাত্র", "বাংলাদেশি", "জঘন্য সাতারু"],
+    typeSpeed: 50,
+    backDelay: 2500,
+    loop: true,
+    shuffle: false
+  });
+  $(".typedcn").typed({
+    strings: ["學過經濟學的數位產品設計師", "正在學習中文的孟加拉人", "不太會游泳的人"],
+    typeSpeed: 50,
+    backDelay: 2500,
+    loop: true,
+    shuffle: false
+  });
+});
+
+
+
+
+
+
+$(function(){
+  //set or get lang
+  $('body').attr('data-current-lang','cn');
+
+  $('[data-switch-lang]').click(function(){
+
+    var newLang = $(this).attr('data-switch-lang'),
+        currentLang = $('body').attr('data-current-lang');
+
+    if (newLang !== currentLang){
+
+      //swap content
+      $('[data-lang="' + newLang + '"]').show();
+      $('[data-lang="' + $('body').attr('data-current-lang') + '"]').hide();
+
+      //set language for future needs
+      $('body').attr('data-current-lang',newLang);
+
+      //optional
+      $('.dropdownTrigger[data-dropdown-id="language"]').text(newLang);
+
+      //Recalculate panels and equal elements
+      $(window).resize();
+    }
+  });
+});
